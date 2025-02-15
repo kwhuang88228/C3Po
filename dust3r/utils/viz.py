fe5110a091035570de11afe0c80e23369b6bd3db
Kuan Wei Huang <kh775@snavely-compute-02.cs.cornell.edu>
2025-02-15 13:43:03 -0500

    updated viz

--- dust3r/utils/viz.py
+++ dust3r/utils/viz.py
@@ -14,57 +14,79 @@ def reverse_ImgNorm(np_array):
 def reverse_CoordNorm(np_array, size):
     return (np_array + 1) * (size - 1) / 2
 
+
+def get_rgbs(pred):
+    rgbs = []
+    for pred_x, pred_y in pred:
+        r = pred_x / 255.0
+        b = pred_y / 255.0
+        rgbs.append((r, 0, b))
+    return rgbs
+
 def get_viz(view1, view2, pred1, pred2):
     def gen_plot(view1, view2, pred1, pred2):
         view1_img = view1["img"].permute(0, 2, 3, 1).cpu().numpy()
         view2_img = view2["img"].permute(0, 2, 3, 1).cpu().numpy()
 
+        # conf = pred2['conf'][0].detach().cpu().numpy()
+        # print(f"conf shape: {conf.shape}; {np.max(conf)}; {np.min(conf)}")
+
         B, image_size, _,  _ = view1_img.shape
-        fig, axes = plt.subplots(B, 3, figsize=(10, B*3))
-        titles = ["gt", "pred", "image"]
+        fig, axes = plt.subplots(B, 4, figsize=(10, B*4))
+        titles = ["gt", "pred", "confidence", "image"]
         for b in range(B):
             if B == 1:
                 view1_img_scaled = reverse_ImgNorm(view1_img[b])
                 axes[0].imshow(view1_img_scaled)
-                view1_points = view1["plan_xys"][b].cpu().numpy()
-                axes[0].scatter(view1_points[:,0], view1_points[:,1], s=5)
+                gt = view1["plan_xys"][b].cpu().numpy()
+                axes[0].scatter(gt[:,0], gt[:,1], s=5)
                 axes[0].set_title(titles[0])
                 
                 axes[1].imshow(view1_img_scaled)    
-                pred2_points = pred2["pts3d_in_other_view"][b].detach().cpu().numpy()
+                pred = pred2["pts3d_in_other_view"][b].detach().cpu().numpy()
                 x_coords = view2["image_xys"][b][:,0].cpu().numpy()
                 y_coords = view2["image_xys"][b][:,1].cpu().numpy()
-                pred2_points = pred2_points[y_coords, x_coords, :2]
-                pred2_points = reverse_CoordNorm(pred2_points, image_size)
-                axes[1].scatter(pred2_points[:,0], pred2_points[:,1], s=5)
+                pred = pred[y_coords, x_coords, :2]
+                pred = reverse_CoordNorm(pred, image_size)
+                axes[1].scatter(pred[:,0], pred[:,1], s=5)
                 axes[1].set_title(titles[1])  
 
+                conf = pred2["conf"][b].detach().cpu().numpy()
+                axes[2].imshow(conf, cmap="jet", interpolation="nearest")   
+                axes[2].set_title(titles[2]) 
+
                 view2_img_scaled = reverse_ImgNorm(view2_img[b])
-                axes[2].imshow(view2_img_scaled)   
-                view2_points = view1["image_xys"][b].cpu().numpy()   
-                axes[2].scatter(view2_points[:,0], view2_points[:,1], s=5) 
-                axes[2].set_title(titles[2])  
+                axes[3].imshow(view2_img_scaled)   
+                image_xys = view1["image_xys"][b].cpu().numpy()   
+                rgbs = get_rgbs(pred)
+                axes[3].scatter(image_xys[:,0], image_xys[:,1], s=1, c=rgbs) 
+                axes[3].set_title(titles[3])      
             else:
                 view1_img_scaled = reverse_ImgNorm(view1_img[b])
                 axes[b, 0].imshow(view1_img_scaled)
-                view1_points = view1["plan_xys"][b].cpu().numpy()
-                axes[b, 0].scatter(view1_points[:,0], view1_points[:,1], s=5)
+                gt = view1["plan_xys"][b].cpu().numpy()
+                axes[b, 0].scatter(gt[:,0], gt[:,1], s=5)
                 axes[b, 0].set_title(titles[0])
                 
                 axes[b, 1].imshow(view1_img_scaled)    
-                pred2_points = pred2["pts3d_in_other_view"][b].detach().cpu().numpy()
+                pred = pred2["pts3d_in_other_view"][b].detach().cpu().numpy()
                 x_coords = view2["image_xys"][b][:,0].cpu().numpy()
                 y_coords = view2["image_xys"][b][:,1].cpu().numpy()
-                pred2_points = pred2_points[y_coords, x_coords, :2]
-                pred2_points = reverse_CoordNorm(pred2_points, image_size)
-                axes[b, 1].scatter(pred2_points[:,0], pred2_points[:,1], s=5)
+                pred = pred[y_coords, x_coords, :2]
+                pred = reverse_CoordNorm(pred, image_size)
+                axes[b, 1].scatter(pred[:,0], pred[:,1], s=5)
                 axes[b, 1].set_title(titles[1])  
 
+                conf = pred2["conf"][b].detach().cpu().numpy()
+                axes[b, 2].imshow(conf, cmap="jet", interpolation="nearest")   
+                axes[b, 2].set_title(titles[2]) 
+
                 view2_img_scaled = reverse_ImgNorm(view2_img[b])
-                axes[b, 2].imshow(view2_img_scaled)   
-                view2_points = view1["image_xys"][b].cpu().numpy()   
-                axes[b, 2].scatter(view2_points[:,0], view2_points[:,1], s=5) 
-                axes[b, 2].set_title(titles[2])   
+                axes[b, 3].imshow(view2_img_scaled)   
+                image_xys = view1["image_xys"][b].cpu().numpy()   
+                rgbs = get_rgbs(pred)
+                axes[b, 3].scatter(image_xys[:,0], image_xys[:,1], s=1, c=rgbs) 
+                axes[b, 3].set_title(titles[3])  
         plt.tight_layout()
         return fig
     viz = gen_plot(view1, view2, pred1, pred2)
--- inference_finetuned.py
+++ inference_finetuned.py
@@ -9,15 +9,19 @@ from dust3r.utils.image import load_megascenes_augmented_images
 from dust3r.utils.viz import get_viz
 import matplotlib.pyplot as plt
 
-def get_inference_viz(plan_path, img_path, xys_path, model, criterion, device):
+def make_batches(plan_path, img_path, xys_path, batch_size):
     plan_xys, image_xys = np.load(xys_path)
     pair = load_megascenes_augmented_images((plan_path, img_path), size=224, plan_xys=plan_xys, image_xys=image_xys)  
     batches = build_dataset([pair], batch_size, num_workers=4, test=True)
+    return batches
 
+def get_inference_viz(batches, model, criterion, device):
+    viz_list = []
     for batch in batches:
         output = loss_of_one_batch(batch, model=model, criterion=criterion, device=device, symmetrize_batch=False, use_amp=False, ret=None)
         viz = get_viz(output["view1"], output["view2"], output["pred1"], output["pred2"])
-        return viz
+        viz_list.append(viz)
+    return viz_list
 
 if __name__ == '__main__':
     device = 'cuda'
@@ -27,16 +31,18 @@ if __name__ == '__main__':
     lr = 0.01
     niter = 300
 
-    model_name = "/share/phoenix/nfs06/S9/kh775/code/dust3r/checkpoints/dust3r_224_500_allplans_dist/checkpoint-best.pth"
+    model_dir = "dust3r_224_500_oneplan2_1gpu_verify"
+    model_name = f"/share/phoenix/nfs06/S9/kh775/code/dust3r/checkpoints/{model_dir}/checkpoint-500.pth"
     model = AsymmetricCroCo3DStereo.from_pretrained(model_name).to(device)
 
     image_dir = "/share/phoenix/nfs06/S9/kh775/dataset/megascenes_augmented_exhaustive"
-    landmark = "Abbaye_de_Moyenmoutier"
-    plan_path = join(image_dir, landmark, "plans", "File:Abbaye de Moyenmoutier-Plan de l'abbaye.jpg")
-    img_path = join(image_dir, landmark, "images", "commons/Abbaye_de_Moyenmoutier/0/pictures/Abbaye de Moyenmoutier en août 2016 (1).jpg")
-    npx_path = "/share/phoenix/nfs06/S9/kh775/code/wsfm/scripts/data/keypoint_localization/data/data_test/coords/097144.npy"
-    viz = get_inference_viz(plan_path, img_path, npx_path, model, criterion, device)
-    viz.savefig("inference_.png")
+    landmark = "Abbatiale_d'Ottmarsheim"
+    plan_path = join(image_dir, landmark, "plans", "File:Ottmarsheim oben.jpg")
+    img_path = join(image_dir, landmark, "images", "commons/Abbatiale_d'Ottmarsheim/0/pictures/Abbatiale Saint Pierre et Saint Paul.jpg")
+    npx_path = "/share/phoenix/nfs06/S9/kh775/code/wsfm/scripts/data/keypoint_localization/old/data_train/one_plan_2/coords/00000000.npy"
+    batches = make_batches(plan_path, img_path, npx_path, batch_size)
+    viz_list = get_inference_viz(batches, model, criterion, device)
+    viz_list[0].savefig(f"inference/{model_dir}/inference_500_train_1.png")
 
     # device = 'cuda'
     # batch_size = 1
